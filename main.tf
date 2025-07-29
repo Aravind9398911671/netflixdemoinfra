9d891df59bef75353b3c6947bf2ca073ac96e938
Aravind <aravindchinna432@gmail.com>
2025-07-29 11:44:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0a7d80731ae1b2435"
 instance_type = "t2.medium"
 key_name = "Aravindkey"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+vpc_security_group_ids = ["sg-0626ba5fb30e73154"]
 tags = {
 Name = var.instance_names[count.index]
 }
