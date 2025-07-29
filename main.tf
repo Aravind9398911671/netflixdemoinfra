b31f76f0f07438cbef34b694e21beba2dc2f6f2c
Aravind <aravindchinna432@gmail.com>
2025-07-29 11:53:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-0a7d80731ae1b2435"
 instance_type = "t2.medium"
-key_name = "Aravindkey"
+key_name = "Arvindkey"
 vpc_security_group_ids = ["sg-0626ba5fb30e73154"]
 tags = {
 Name = var.instance_names[count.index]
